13749880ed09d6996002fee34d3bd3a2f5374fac
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-16 23:21:20 +0700

    add generic repository methods for counting, creating, and finding entities by email

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,21 @@
 package repository
+
+import "gorm.io/gorm"
+
+type Repository[T any] struct {
+	DB *gorm.DB
+}
+
+func (r *Repository[T]) CountByField(db *gorm.DB, field string, value interface{}) (int64, error) {
+	var count int64
+	err := db.Unscoped().Model(new(T)).Where(field+" = ?", value).Count(&count).Error
+	return count, err
+}
+
+func (r *Repository[T]) Create(db *gorm.DB, entity *T) error {
+	return db.Create(entity).Error
+}
+
+func (r *Repository[T]) FindByEmail(db *gorm.DB, entity *T, email string) error {
+	return db.Where("email = ?", email).Take(entity).Error
+}

c62bd9c5a662639994eca6283ce4f9a9e1245634
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-16 20:00:33 +0700

    add configuration for Fiber, GORM, Logrus, Redis, and Validator

--- internal/config/fiber.go
+++ internal/config/fiber.go
@@ -1,1 +1,36 @@
 package config
+
+import (
+	"github.com/abdisetiakawan/go-ecommerce/internal/model"
+	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
+)
+
+func NewFiber(config *viper.Viper) *fiber.App {
+    var app = fiber.New(fiber.Config{
+        AppName:      config.GetString("app.name"),
+        ErrorHandler: NewErrorHandler(),
+        Prefork:      config.GetBool("web.prefork"),
+    })
+
+    return app
+}
+
+func NewErrorHandler() fiber.ErrorHandler {
+    return func(ctx *fiber.Ctx, err error) error {
+        code := fiber.StatusInternalServerError
+        message := err.Error()
+
+        switch e := err.(type) {
+        case *model.ApiError:
+            code = e.StatusCode
+            message = e.Message
+        case *fiber.Error:
+            code = e.Code
+            message = e.Message
+        }
+        return ctx.Status(code).JSON(fiber.Map{
+            "errors": message,
+        })
+    }
+}
--- internal/config/redis.go
+++ internal/config/redis.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func NewRedisClient(viper *viper.Viper, log *logrus.Logger) *redis.Client{
+	addr := viper.GetString("redis.addr")
+	password := viper.GetString("redis.password")
+	db := viper.GetInt("redis.db")
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+	})
+
+	_, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		log.Fatalf("failed to connect to redis: %v", err)
+	}
+
+	return client
+}

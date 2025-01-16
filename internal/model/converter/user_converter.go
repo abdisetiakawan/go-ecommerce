1c7a58a464c4e29b5a83e7f7206a42a2a1a4aeb6
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-16 23:32:14 +0700

    add user authentication response converter

--- internal/model/converter/user_converter.go
+++ internal/model/converter/user_converter.go
@@ -1,1 +1,18 @@
 package converter
+
+import (
+	"github.com/abdisetiakawan/go-ecommerce/internal/entity"
+	"github.com/abdisetiakawan/go-ecommerce/internal/model"
+)
+
+func AuthToResponse(user *entity.User) *model.AuthResponse {
+	return &model.AuthResponse{
+		UserUUID:    user.UserUUID,
+		Username:    user.Username,
+		Name:        user.Name,
+		Email:       user.Email,
+		Role:        user.Role,
+		AccessToken: user.AccessToken,
+		RefreshToken: user.RefreshToken,
+	}
+}

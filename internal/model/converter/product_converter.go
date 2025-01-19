76c7134264836450ba35cfa90635e3c49526946b
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-19 23:02:49 +0700

    feat: add ProductToResponse converter function for product entity

--- internal/model/converter/product_converter.go
+++ internal/model/converter/product_converter.go
@@ -1,1 +1,18 @@
 package converter
+
+import (
+	"github.com/abdisetiakawan/go-ecommerce/internal/entity"
+	"github.com/abdisetiakawan/go-ecommerce/internal/model"
+)
+
+func ProductToResponse(product *entity.Product) *model.ProductResponse {
+	return &model.ProductResponse{
+		ProductUUID: product.ProductUUID,
+		StoreID:     product.StoreID,
+		ProductName: product.ProductName,
+		Description: product.Description,
+		Price:       product.Price,
+		Stock:       product.Stock,
+		Category:    product.Category,
+	}
+}

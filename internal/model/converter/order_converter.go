72919e31213d49fdb7cb91ccfff65ab6119203d4
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-21 00:22:38 +0700

    feat: create model for request and response order, create converter for structured response

--- internal/model/converter/order_converter.go
+++ internal/model/converter/order_converter.go
@@ -1,1 +1,37 @@
 package converter
+
+import (
+	"github.com/abdisetiakawan/go-ecommerce/internal/entity"
+	"github.com/abdisetiakawan/go-ecommerce/internal/model"
+)
+
+func OrderToResponse(order *entity.Order) *model.OrderResponse {
+    items := make([]model.OrderItemResponse, len(order.Items))
+
+    for i, item := range order.Items {
+        items[i] = model.OrderItemResponse{
+            OrderItemUuid: item.OrderItemUUID,
+            Quantity:  item.Quantity,
+        }
+    }
+
+    return &model.OrderResponse{
+        OrderUUID:  order.OrderUUID,
+        TotalPrice: order.TotalPrice,
+        Status:     order.Status,
+        Items:      items,
+        Shipping: model.ShippingResponse{
+            ShippingUUID: order.Shipping.ShippingUUID,
+            Address:      order.Shipping.Address,
+            City:         order.Shipping.City,
+            Province:     order.Shipping.Province,
+            PostalCode:   order.Shipping.PostalCode,
+            Status:       order.Shipping.Status,
+        },
+        Payment: model.PaymentResponse{
+            PaymentUUID:  order.Payment.PaymentUUID,
+            PaymentMethod: order.Payment.Method,
+            Status:        order.Payment.Status,
+        },
+    }
+}
--- internal/model/order_model.go
+++ internal/model/order_model.go
@@ -1,1 +1,53 @@
 package model
+
+type CreateOrder struct {
+	UserID          uint                   `json:"-"`
+	Items           []OrderItemRequest     `json:"items" validate:"required,dive"`
+	ShippingAddress ShippingAddressRequest `json:"shipping_address" validate:"required"`
+	Payments        PaymentRequest         `json:"payments" validate:"required"`
+}
+
+type OrderItemRequest struct {
+	ProductUUID string `json:"product_uuid" validate:"required"`
+	Quantity    int    `json:"quantity" validate:"required,gte=1"`
+}
+
+type ShippingAddressRequest struct {
+	Address    string `json:"address" validate:"required"`
+	City       string `json:"city" validate:"required"`
+	Province   string `json:"province" validate:"required"`
+	PostalCode string `json:"postal_code" validate:"required,min=5,max=5"`
+}
+
+type PaymentRequest struct {
+	PaymentMethod string `json:"payment_method" validate:"required,oneof=cash transfer"`
+}
+
+type OrderResponse struct {
+	OrderUUID  string              `json:"order_uuid"`
+	TotalPrice float64             `json:"total_price"`
+	Status     string              `json:"status"`
+	Items      []OrderItemResponse `json:"items"`
+	Shipping   ShippingResponse    `json:"shipping"`
+	Payment    PaymentResponse     `json:"payment"`
+}
+
+type OrderItemResponse struct {
+	OrderItemUuid string `json:"order_item_uuid"`
+	Quantity  int  `json:"quantity"`
+}
+
+type ShippingResponse struct {
+	ShippingUUID string `json:"shipping_uuid"`
+	Address      string `json:"address"`
+	City         string `json:"city"`
+	Province     string `json:"province"`
+	PostalCode   string `json:"postal_code"`
+	Status       string `json:"status"`
+}
+
+type PaymentResponse struct {
+	PaymentUUID   string `json:"payment_uuid"`
+	PaymentMethod string `json:"payment_method"`
+	Status        string `json:"status"`
+}

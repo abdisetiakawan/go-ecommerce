f4bfc33a2ef7066bcdde9e26dd82216b4efec072
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-21 00:21:32 +0700

    implemented route for buyer

--- internal/delivery/route/route_config.go
+++ internal/delivery/route/route_config.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"github.com/abdisetiakawan/go-ecommerce/internal/delivery/http"
+	"github.com/abdisetiakawan/go-ecommerce/internal/delivery/http/buyer"
 	"github.com/abdisetiakawan/go-ecommerce/internal/delivery/http/seller"
 	"github.com/abdisetiakawan/go-ecommerce/internal/delivery/http/user"
 	"github.com/gofiber/fiber/v2"
@@ -11,6 +12,7 @@ type RouteConfig struct {
 	App *fiber.App
 	AuthController *http.AuthController
 	SellerController *seller.SellerController
+	BuyerController *buyer.BuyerController
 	UserController *user.UserController
 	AuthMiddleware    fiber.Handler
 }
@@ -18,5 +20,6 @@ type RouteConfig struct {
 func (c *RouteConfig) Setup() {
 	SetupAuthRoute(c.App, c.AuthController)
 	SetupUserRoute(c, c.App, c.UserController)
+	SetupBuyerRoute(c, c.App, c.BuyerController)
 	SetupSellerRoute(c, c.App, c.SellerController)
 }

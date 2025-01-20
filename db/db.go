d96b4b20da1ced61b9a59839ed695f7ceaa3b309
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-20 18:30:42 +0700

    feat: refactor database migration logic into separate Migrate function with enhanced error logging

--- db/db.go
+++ db/db.go
@@ -6,9 +6,47 @@ import (
 
 	"github.com/abdisetiakawan/go-ecommerce/internal/config"
 	"github.com/abdisetiakawan/go-ecommerce/internal/entity"
+	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+
+func Migrate(db *gorm.DB, log *logrus.Logger) {
+    if err := db.AutoMigrate(&entity.User{}); err != nil {
+        log.Fatalf("failed to migrate User entity: %v", err)
+    }
+
+    if err := db.AutoMigrate(&entity.Profile{}); err != nil {
+        log.Fatalf("failed to migrate Profile entity: %v", err)
+    }
+
+    if err := db.AutoMigrate(&entity.Store{}); err != nil {
+        log.Fatalf("failed to migrate Store entity: %v", err)
+    }
+
+    if err := db.AutoMigrate(&entity.Product{}); err != nil {
+        log.Fatalf("failed to migrate Product entity: %v", err)
+    }
+
+    if err := db.AutoMigrate(&entity.Order{}); err != nil {
+        log.Fatalf("failed to migrate Order entity: %v", err)
+    }
+
+    if err := db.AutoMigrate(&entity.OrderItem{}); err != nil {
+        log.Fatalf("failed to migrate OrderItem entity: %v", err)
+    }
+
+    if err := db.AutoMigrate(&entity.Payment{}); err != nil {
+        log.Fatalf("failed to migrate Payment entity: %v", err)
+    }
+
+    if err := db.AutoMigrate(&entity.Shipping{}); err != nil {
+        log.Fatalf("failed to migrate Shipping entity: %v", err)
+    }
+
+    log.Info("Migration completed successfully")
+}
 
 func main() {
 	viperConfig := config.NewViper()
@@ -33,9 +71,6 @@ func main() {
 		log.Fatalf("failed to connect to database : %v", err)
 	}
 
-	if err := db.AutoMigrate(&entity.User{}, &entity.Profile{}, &entity.Store{}, &entity.Product{}); err != nil {
-		log.Fatalf("failed to migrate database : %v", err)
-	}
-
-	fmt.Println("Migration completed successfully")
+	logger := config.NewLogger(viperConfig)
+	Migrate(db, logger)
 }

07e1cf7cb35724d983eacccace08357b8837a9a9
abdisetiakawan <bedikadiryt@gmail.com>
2025-01-22 20:46:03 +0700

    feat: implements the change password endpoint and the update profile endpoint to SetupUserRoute

--- internal/delivery/route/user_route.go
+++ internal/delivery/route/user_route.go
@@ -9,4 +9,6 @@ func SetupUserRoute(r *RouteConfig, app *fiber.App, userController *user.UserCon
 	app.Use(r.AuthMiddleware)
 	app.Post("/api/user/profile", userController.CreateProfile)
 	app.Get("/api/user/profile", userController.GetProfile)
+	app.Put("/api/user/profile", userController.UpdateProfile)
+	app.Patch("/api/user/password", userController.ChangePassword)
 }

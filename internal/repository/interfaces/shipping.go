1cadca0966469895dc6dcfc17e63c839f0ad2882
abdisetiakawan <bedikadiryt@gmail.com>
2025-02-03 22:48:01 +0700

    Refactors payment and shipping creation
    Consumes messages from Kafka topics to create payments and shipping information.
    Adds Kafka consumer dependency to payment and shipping repositories.
    Updates repository interfaces to reflect changes.

--- internal/repository/interfaces/shipping.go
+++ internal/repository/interfaces/shipping.go
@@ -3,6 +3,6 @@ package interfaces
 import "github.com/abdisetiakawan/go-ecommerce/internal/entity"
 
 type ShippingRepository interface {
-	CreateShipping(shipping *entity.Shipping) error
+	CreateShipping() error
 	UpdateShipping(shipping *entity.Shipping) error
 }

24ff8b5d986ed343589d9f88a6a8b35a840cd11d
abdisetiakawan <bedikadiryt@gmail.com>
2025-02-03 22:48:27 +0700

    Integrates Kafka for payment and shipping
    Adds Kafka producer and consumer for asynchronous processing of payment and shipping events.
    Updates `BootstrapConfig` to include Kafka producer and consumer instances.
    Introduces new repositories for payment and shipping that leverage Kafka for event handling.
    The changes enhance system scalability and resilience by decoupling payment and shipping processes from the main application flow.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/abdisetiakawan/go-ecommerce/internal/config"
 	"github.com/abdisetiakawan/go-ecommerce/internal/helper"
+	"github.com/abdisetiakawan/go-ecommerce/internal/repository"
 )
 
 func main() {
@@ -15,14 +16,32 @@ func main() {
     validator := config.NewValidator(viperConfig)
     jwt := helper.NewJWTHelper(viperConfig)
     uuid := helper.NewUUIDHelper()
-    
+
+    kafkaProducer, err := helper.NewKafkaProducer(viperConfig)
+    if err != nil {
+        logger.Fatal(err)
+        panic(err)
+    }
+    kafkaConsumer, err := helper.NewKafkaConsumer(viperConfig)
+    if err != nil {
+        logger.Fatal(err)
+        panic(err)
+    }
     app := config.NewFiber(viperConfig)
 
-    config.Bootstrap(&config.BootstrapConfig{DB: db, App: app, Log: logger, Validate: validator, Config: viperConfig, Jwt: jwt, UserUUID: uuid})
+    config.Bootstrap(&config.BootstrapConfig{DB: db, App: app, Log: logger, Validate: validator, Config: viperConfig, Jwt: jwt, UserUUID: uuid, KafkaProducer: kafkaProducer, KafkaConsumer: kafkaConsumer})
+	go func() {
+		paymentRepository := repository.NewPaymentRepository(db, kafkaConsumer)
+		paymentRepository.CreatePayment()
+	}()
 
+	go func() {
+		shippingRepository := repository.NewShippingRepository(db, kafkaConsumer)
+		shippingRepository.CreateShipping()
+	}()
     port := viperConfig.GetInt("web.port")
     logger.Infof("Starting server on port %d", port)
-    err := app.Listen(fmt.Sprintf(":%d", port))
+    err = app.Listen(fmt.Sprintf(":%d", port))
     if err != nil {
         logger.Fatalf("Failed to start server: %v", err)
     }
--- internal/config/app.go
+++ internal/config/app.go
@@ -22,6 +22,8 @@ type BootstrapConfig struct {
 	Config   *viper.Viper
 	Jwt      *helper.JwtHelper
 	UserUUID *helper.UUIDHelper
+	KafkaProducer *helper.KafkaProducer
+	KafkaConsumer *helper.KafkaConsumer
 }
 
 func Bootstrap(config *BootstrapConfig) {
@@ -30,12 +32,12 @@ func Bootstrap(config *BootstrapConfig) {
 	orderRepository := repository.NewOrderRepository(config.DB)
 	productRepository := repository.NewProductRepository(config.DB)
 	storeRepository := repository.NewStoreRepository(config.DB)
-	shippingRepository := repository.NewShippingRepository(config.DB)
-	paymentRepository := repository.NewPaymentRepository(config.DB)
+	shippingRepository := repository.NewShippingRepository(config.DB, config.KafkaConsumer)
+	paymentRepository := repository.NewPaymentRepository(config.DB, config.KafkaConsumer)
 
 	userUseCase := usecase.NewUserUseCase(config.DB, config.Log, config.Validate, userRepository, config.UserUUID, config.Jwt)
 	profileUseCase := usecase.NewProfileUseCase(config.DB, config.Log, config.Validate, profileRepository)
-	orderUseCase := usecase.NewOrderUseCase(config.DB, config.Log, config.Validate, orderRepository, productRepository, paymentRepository, shippingRepository, storeRepository, config.UserUUID)
+	orderUseCase := usecase.NewOrderUseCase(config.DB, config.Log, config.Validate, orderRepository, productRepository, paymentRepository, shippingRepository, storeRepository, config.UserUUID, config.KafkaProducer)
 	productUseCase := usecase.NewProductUseCase(config.DB, config.Log, config.Validate, productRepository, storeRepository, config.UserUUID)
 	storeUseCase := usecase.NewStoreUseCase(config.DB, config.Log, config.Validate, storeRepository, config.UserUUID)
 	shippingUseCase := usecase.NewShippingUseCase(config.DB, config.Log, config.Validate, shippingRepository, storeRepository, orderRepository, config.UserUUID)

84c95017389fb91b95941b301d1514d88b5903bf
abdisetiakawan <bedikadiryt@gmail.com>
2025-02-03 22:46:00 +0700

    Adds Kafka producer functionality
    Implements a Kafka producer using Sarama.  The producer configuration is now loaded from Viper.  This allows for flexible and configurable interaction with Kafka brokers.

---
+++ internal/kafka/kafka.go
@@ -0,0 +1,29 @@
+package kafka
+
+import (
+	"github.com/IBM/sarama"
+	"github.com/spf13/viper"
+)
+
+type KafkaConfig struct {
+    Brokers []string
+}
+
+func NewKafkaConfig(viper *viper.Viper) *KafkaConfig {
+    return &KafkaConfig{
+        Brokers: viper.GetStringSlice("kafka.brokers"),
+    }
+}
+
+func NewKafkaProducer(config *KafkaConfig) (sarama.SyncProducer, error) {
+    saramaConfig := sarama.NewConfig()
+    saramaConfig.Producer.Return.Successes = true
+    saramaConfig.Producer.Return.Errors = true
+
+    producer, err := sarama.NewSyncProducer(config.Brokers, saramaConfig)
+    if err != nil {
+        return nil, err
+    }
+
+    return producer, nil
+}

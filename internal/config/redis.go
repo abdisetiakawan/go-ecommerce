ee149d83b22e957bb472feac87b46de6fd9c0b20
abdisetiakawan <bedikadiryt@gmail.com>
2025-02-21 20:48:33 +0700

    delete unused file and change redis configuration

--- internal/config/redis.go
+++ internal/config/redis.go
@@ -2,16 +2,16 @@ package config
 
 import (
 	"context"
+	"log"
 
 	"github.com/go-redis/redis/v8"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
-func NewRedisClient(viper *viper.Viper, log *logrus.Logger) *redis.Client{
-	addr := viper.GetString("redis.addr")
-	password := viper.GetString("redis.password")
-	db := viper.GetInt("redis.db")
+func NewRedisClient(viper *viper.Viper) *redis.Client{
+	addr := viper.GetString("REDIS_ADDR")
+	password := viper.GetString("REDIS_PASS")
+	db := viper.GetInt("REDIS_DB")
 
 	client := redis.NewClient(&redis.Options{
 		Addr:     addr,

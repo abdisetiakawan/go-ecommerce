0d6855e87fa50710360f3ff41596a880a7839dc1
abdisetiakawan <bedikadiryt@gmail.com>
2025-05-09 03:12:11 +0700

    Refactors: Simplifies payment repository
    Removes unnecessary methods and dependencies from the payment repository.
    This change streamlines the repository by removing create, cancel, and checkout payment methods as well as the kafka dependency. The repository now focuses solely on updating payment information.

--- internal/repository/interfaces/payment.go
+++ internal/repository/interfaces/payment.go
@@ -3,8 +3,5 @@ package interfaces
 import "github.com/abdisetiakawan/go-ecommerce/internal/entity"
 
 type PaymentRepository interface {
-	CreatePayment() error
 	UpdatePayment(payment *entity.Payment) error
-	CancelPayment() error
-	CheckoutPayment() error
 }
--- internal/repository/payment.go
+++ internal/repository/payment.go
@@ -1,122 +1,20 @@
 package repository
 
 import (
-	"context"
-	"encoding/json"
-
 	"github.com/abdisetiakawan/go-ecommerce/internal/entity"
-	"github.com/abdisetiakawan/go-ecommerce/internal/helper"
-	eventmodel "github.com/abdisetiakawan/go-ecommerce/internal/model/event_model"
 	"github.com/abdisetiakawan/go-ecommerce/internal/repository/interfaces"
-	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
 
 type PaymentRepository struct {
-	kafka *helper.KafkaConsumer
 	DB *gorm.DB
 }
 
-func NewPaymentRepository(DB *gorm.DB, kafka *helper.KafkaConsumer) interfaces.PaymentRepository {
-	return &PaymentRepository{kafka,DB}
-}
-
-func (r *PaymentRepository) CreatePayment() error {
-    consumer, err := r.kafka.Consume(context.Background(), "create_payment_topic")
-    if err != nil {
-        return err
-    }
-    defer consumer.Close()
-
-    for {
-        select {
-        case msg := <-consumer.Messages():
-            var paymentMessage eventmodel.PaymentMessage
-            err := json.Unmarshal(msg.Value, &paymentMessage)
-            if err != nil {
-                logrus.WithError(err).Error("Failed to unmarshal payment message")
-                continue
-            }
-
-            payment := &entity.Payment{
-                PaymentUUID: paymentMessage.PaymentUUID,
-                Amount:      paymentMessage.Amount,
-                Method:      paymentMessage.Method,
-                OrderID:     paymentMessage.OrderID,
-                Status:      paymentMessage.Status,
-            }
-
-            if err := r.DB.Create(payment).Error; err != nil {
-                logrus.WithError(err).Error("Failed to create payment")
-                continue
-            }
-
-        case err := <-consumer.Errors():
-            logrus.WithError(err).Error("Failed to consume payment topic")
-        }
-    }
+func NewPaymentRepository(DB *gorm.DB) interfaces.PaymentRepository {
+	return &PaymentRepository{DB: DB}
 }
 
 func (r *PaymentRepository) UpdatePayment(payment *entity.Payment) error {
 	return r.DB.Save(payment).Error
 }
 
-func (r *PaymentRepository) CancelPayment() error {
-    consumer, err := r.kafka.Consume(context.Background(), "cancel_payment_topic")
-    if err != nil {
-        return err
-    }
-    defer consumer.Close()
-
-    for {
-        select {
-        case msg := <-consumer.Messages():
-            var paymentMessage eventmodel.PaymentMessage
-            err := json.Unmarshal(msg.Value, &paymentMessage)
-            if err != nil {
-                logrus.WithError(err).Error("Failed to unmarshal payment message")
-                continue
-            }
-
-            if err := r.DB.Model(&entity.Payment{}).
-                Where("order_id = ?", paymentMessage.OrderID).
-                Update("status", "cancelled").Error; err != nil {
-                logrus.WithError(err).Error("Failed to update order status")
-            }
-        
-
-        case err := <-consumer.Errors():
-            logrus.WithError(err).Error("Failed to consume payment topic")
-        }
-    }
-}
-
-func (r *PaymentRepository) CheckoutPayment() error {
-    consumer, err := r.kafka.Consume(context.Background(), "checkout_payment_topic")
-    if err != nil {
-        return err
-    }
-    defer consumer.Close()
-
-    for {
-        select {
-        case msg := <-consumer.Messages():
-            var paymentMessage eventmodel.PaymentMessage
-            err := json.Unmarshal(msg.Value, &paymentMessage)
-            if err != nil {
-                logrus.WithError(err).Error("Failed to unmarshal payment message")
-                continue
-            }
-
-            if err := r.DB.Model(&entity.Payment{}).
-                Where("order_id = ?", paymentMessage.OrderID).
-                Update("status", paymentMessage.Status).Error; err != nil {
-                logrus.WithError(err).Error("Failed to update order status")
-            }
-        
-
-        case err := <-consumer.Errors():
-            logrus.WithError(err).Error("Failed to consume payment topic")
-        }
-    }
-}
